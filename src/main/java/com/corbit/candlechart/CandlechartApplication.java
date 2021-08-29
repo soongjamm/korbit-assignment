c3c5c05d7487288bdf06a117c531823fed550bd1
soongjamm <kimstz0303@gmail.com>
2021-08-29 18:49:29 +0900

    initial commit

---
+++ src/main/java/com/corbit/candlechart/CandlechartApplication.java
@@ -0,0 +1,13 @@
+package com.corbit.candlechart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandlechartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandlechartApplication.class, args);
+	}
+
+}
